9451ed308f9f0f6ad4eeceafa997d56b3763a917
UberMouse <ubermouse894@gmail.com>
2013-09-04 20:34:05 +1200

    Removed console.log call

--- static/script.js
+++ static/script.js
@@ -2,7 +2,6 @@ $("#bushingSubmit").click(function(e) {
 	e.preventDefault();
 	var weight = $("#weight").val();
 	var weightType = $("#weightType").val();
-	console.log(weight);
 	var url = "query?weight="+weight+"&weight_type=" + weightType;
 
 	$("#content").load(url + " #content");
