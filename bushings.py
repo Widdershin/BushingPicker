de8ea8de5aa9aa96e749f47853eaf1f5351b591c
Widdershin <ncwjohnstone@gmail.com>
2013-09-05 12:27:37 +1200

    Changed bushing pairs to be in a class and fixed dupe results

--- bushings.py
+++ bushings.py
@@ -1,5 +1,19 @@
 from flask import Flask, render_template, request
 
+class BushingPair(object):
+  """An object to represent a pair of bushings"""
+  def __init__(self, weight, label):
+    super(BushingPair, self).__init__()
+    self.weight = weight
+    self.boardside = weight_to_duro(self.weight, boardside=True)
+    self.roadside = weight_to_duro(self.weight, boardside=False)
+    self.label = label
+    self.bushings = [self.roadside, self.boardside]
+
+  def compare_pairs(self, other):
+    """Returns true if self and other have the same duro bushings"""
+    return (self.boardside == other.boardside and self.roadside == other.roadside)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -27,10 +41,24 @@ def query():
 
   bushing_pairs = []
 
-  columns = [("Soft", -5), ("Medium", 0), ("Hard", 5)]
+  offset = 5
+
+  min_duro = sorted(bg_colors)[0]
+  max_duro = sorted(bg_colors)[-1]
+
+  columns = [("Soft", -1), ("Medium", 0), ("Hard", 1)]
 
   for column in columns:
-    bushing_pairs.append([[weight_to_duro(weight + column[1], boardside=False), weight_to_duro(weight + column[1], boardside=True)], column[0]])
+    bushing_pairs.append(BushingPair(weight + column[1] * offset, column[0]))
+
+  items_to_test = [0, 2]
+
+  for item in items_to_test:
+    i = 1
+    while bushing_pairs[item].compare_pairs(bushing_pairs[1]) and bushing_pairs[item].roadside != min_duro and bushing_pairs[item].boardside != max_duro:
+      print "changing {}".format(item)
+      bushing_pairs[item] = BushingPair(weight + (offset + i) * columns[item][1], columns[item][0])
+      i += 2
 
   return render_template("main.html", show_bushings=True, invalid=False, bushing_pairs=bushing_pairs, bg_colors=bg_colors, weight = int(request.values['weight']), weight_type = weight_type, text_colors=text_colors)
 
@@ -41,7 +69,7 @@ def weight_to_duro(weight, boardside=True):
   if not boardside:
     weight -= 5
 
-  weight = weight - 40
+  weight -= 40
   if weight < 0:
     weight = 0
   approx_duro = ((weight) ** 0.66 ) + 79
